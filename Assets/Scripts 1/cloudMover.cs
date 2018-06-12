a5627838d010111b90167b0984a6353f3fdc2aa2
Taiga Maki <a14dc152@dhw.ac.jp>
2018-06-12 16:48:15 +0900

    aaa

---
+++ Assets/Scripts 1/cloudMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// this script was intended to move the clouds
+// since the game produces a lot of cpu workload (due to the size of gameobjects)
+// I decided not to activate it
+
+public class cloudMover : MonoBehaviour {
+
+	float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+		position.z = position.z - speed*Time.deltaTime;
+		if (position.z < 2)
+			position.z = 16;
+		transform.position = position;
+
+	}
+}
